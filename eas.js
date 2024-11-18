b687d974f0802f4a7a08920e68fbe9cf0c3059b9
Carl Laguerre <gglaguerre1@gmail.com>
2024-11-17 19:02:09 -0500

    page sucesfully displays a 16x16 grid made up of 256 square divs

--- eas.js
+++ eas.js
@@ -2,7 +2,9 @@ console.log("hi");
 let containerDiv = document.querySelector(".container");
 //create gridSquare then append to containerDiv x amount of times?
 //put 16x16 divs on the DOM, then flexbox for appearance
-let gridSquareDiv = document.createElement("div");
-gridSquareDiv.className = "grid-square";
-
-containerDiv.appendChild(gridSquareDiv);
+//create second set of grid squares?
+for (let i = 0; i < 256; i++) {
+  let gridSquareDiv = document.createElement("div");
+  gridSquareDiv.className = "grid-square";
+  containerDiv.appendChild(gridSquareDiv);
+}
