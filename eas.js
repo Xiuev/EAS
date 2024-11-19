64a9f7ccd23ec703d2e32dd2466b26aa43831d43
Carl Laguerre <gglaguerre1@gmail.com>
2024-11-18 23:19:11 -0500

    created eventlistner with mouseover effect to change gridbox color when mouse hovers

--- eas.js
+++ eas.js
@@ -1,10 +1,16 @@
 console.log("hi");
 let containerDiv = document.querySelector(".container");
-//create gridSquare then append to containerDiv x amount of times?
-//put 16x16 divs on the DOM, then flexbox for appearance
-//create second set of grid squares?
+// 16 x16 grid
 for (let i = 0; i < 256; i++) {
   let gridSquareDiv = document.createElement("div");
   gridSquareDiv.className = "grid-square";
   containerDiv.appendChild(gridSquareDiv);
+}
+
+let gridSquares = Array.from(document.querySelectorAll(".grid-square"));
+// convert gridSquare nodelist --> array --> loop thru array elements and attach event listner to each item
+for (let i = 0; i < gridSquares.length; i++) {
+  gridSquares[i].addEventListener("mouseover", function (e) {
+    e.target.style.backgroundColor = "pink";
+  });
 }
