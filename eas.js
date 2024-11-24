153d5b27b5c6fad29fe8f871abab336f5ac811ac
Carl Laguerre <gglaguerre1@gmail.com>
2024-11-24 11:52:49 -0500

    completed function to remove enitre Grid

--- eas.js
+++ eas.js
@@ -4,7 +4,7 @@ let numBtn = document.querySelector("#pop-up");
 let testButton = document.querySelector("#test-btn");
 //get promt response from user and convert to int
 
-testButton.addEventListener("click", removeGrid);
+testButton.addEventListener("click", removeAllGrid);
 numBtn.addEventListener("click", newUserGrid);
 // 16 x16 grid
 for (let i = 0; i < 256; i++) {
@@ -12,7 +12,7 @@ for (let i = 0; i < 256; i++) {
   gridSquareDiv.className = "grid-square";
   containerDiv.appendChild(gridSquareDiv);
 }
-
+//turn into array to iterate thru and attach event listner for trail
 let gridSquares = Array.from(document.querySelectorAll(".grid-square"));
 // convert gridSquare nodelist --> array --> loop thru array elements and attach event listner to each item
 for (let i = 0; i < gridSquares.length; i++) {
@@ -28,10 +28,33 @@ function newUserGrid() {
   let userResponse = parseInt(prompt("How many squares for new grid?"));
   if (Number.isInteger(userResponse) === false) {
     alert("Please enter a valid number!");
+  } else {
+    removeGrid();
+    let userResponseGridValue = userResponse * userResponse;
+    for (let i = 0; i < userResponseGridValue; i++) {
+      //style userSquareDiv in css to make it appear.
+      let userSquareDiv = document.createElement("div");
+      userSquareDiv.className = "userSquareDiv";
+      containerDiv.appendChild(userSquareDiv);
+    }
+    console.log(userResponseGridValue);
   }
 }
 function removeGrid() {
   for (let i = 0; i < gridSquares.length; i++) {
     gridSquares[i].remove();
   }
+}
+
+function removeUserGrid() {
+  let userGridsRemove = document.querySelectorAll(".userSquareDiv");
+}
+
+function removeAllGrid() {
+  //get all child elements into an array --> loop thru array then delte
+  let gridDivChild = Array.from(containerDiv.childNodes);
+  for (let i = 0; i < gridDivChild.length; i++) {
+    gridDivChild[i].remove();
+  }
+  console.log(gridDivChild);
 }
